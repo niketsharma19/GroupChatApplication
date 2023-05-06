59df5e78219e5897ae00ce2667e2ecc1769ba505
Niket Sharma <niketsharma22@gmail.com>
2023-05-06 17:01:36 +0530

    Initial commit for Spring Boot Websockets Group Chat Application

---
+++ src/test/java/com/chat/websocket/chatWebsocket/ChatWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.websocket.chatWebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
